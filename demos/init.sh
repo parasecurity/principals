3af4a6eb247224bf698b2fbeec4313f08585393d
GNtousakis <greco1996@gmail.com>
2021-04-27 15:58:10 +0300

    Minor updates

--- demos/init.sh
+++ demos/init.sh
@@ -26,30 +26,18 @@ setupCommonImages()
 	docker push localhost:5000/tsi-flow-control:common &> /dev/null
 	docker rmi localhost:5000/tsi-flow-control:common &> /dev/null
 
+        #analyser
+	docker build common/images/analyser -t tsi-analyser:common &> /dev/null
+	docker tag tsi-analyser:common localhost:5000/tsi-analyser:common &> /dev/null
+	docker push localhost:5000/tsi-analyser:common &> /dev/null
+	docker rmi localhost:5000/tsi-analyser:common &> /dev/null
+
 	if [[ "$CLEAN" == "True" ]]
 	then
 		docker rmi antrea-tsi:v1.0.0 &> /dev/null
 		docker rmi tsi-dga:common &> /dev/null
 		docker rmi tsi-flow-control:common &> /dev/null
-	fi
-}
-
-setupDemo6Images()
-{
-	docker build demo6/images/logger -t tsi-logger:demo6 &> /dev/null
-	docker tag tsi-logger:demo6 localhost:5000/tsi-logger:demo6 &> /dev/null
-	docker push localhost:5000/tsi-logger:demo6 &> /dev/null
-	docker rmi localhost:5000/tsi-logger:demo6 &> /dev/null
-
-	docker build demo6/images/analyser -t tsi-analyser:demo6 &> /dev/null
-	docker tag tsi-analyser:demo6 localhost:5000/tsi-analyser:demo6 &> /dev/null
-	docker push localhost:5000/tsi-analyser:demo6 &> /dev/null
-	docker rmi localhost:5000/tsi-analyser:demo6 &> /dev/null
-
-	if [[ "$CLEAN" == "True" ]]
-	then
-		docker rmi tsi-logger:demo6 &> /dev/null
-		docker rmi tsi-analyser:demo6 &> /dev/null
+		docker rmi tsi-analyser:common &> /dev/null
 	fi
 }
 
@@ -85,9 +73,6 @@ fi
 
 msg "Creating common images"
 setupCommonImages
-
-msg "Creating demo 6 images"
-setupDemo6Images
 
 msg "Creating demo 7 images"
 setupDemo7Images
