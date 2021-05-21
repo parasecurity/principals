f80b70344f6ecbda3ab83ac80a25752166272707
GNtousakis <gntousakis@isc.tuc.gr>
2021-05-21 17:44:55 +0300

    Add first versions of api/tools images

---
+++ demos/common/images/tools/httpCanary.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	args struct {
+		server    *string
+		threshold *int
+		logPath   *string
+	}
+)
+
+func timeGet(url string) {
+	t := http.DefaultTransport.(*http.Transport).Clone()
+	t.MaxIdleConns = 100
+	t.MaxConnsPerHost = 100
+	t.MaxIdleConnsPerHost = 100
+
+	for {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println("Canary connection timeout")
+			}
+		}()
+		httpClient := &http.Client{
+			Timeout:   1 * time.Second,
+			Transport: t,
+		}
+		start := time.Now()
+		r, err := httpClient.Get(*args.server)
+		r.Body.Close()
+		interval := time.Since(start)
+		log.Println("Response in :", interval)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if interval > time.Duration(*args.threshold)*time.Microsecond {
+			log.Println("Threshold passed:", interval)
+		}
+		time.Sleep(time.Second)
+		httpClient.CloseIdleConnections()
+	}
+}
+
+func init() {
+	args.server = flag.String("conn", "http://147.27.39.116:8080/health/", "The server url e.g. http://147.27.39.116:8080/health/")
+	args.threshold = flag.Int("t", 1000, "The time threshold in μs")
+	args.logPath = flag.String("lp", "./canary.log", "The path to the log file")
+	flag.Parse()
+
+	// open log file
+	logFile, err := os.OpenFile(*args.logPath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.SetFlags(log.Ldate | log.Ltime)
+	log.SetOutput(logFile)
+
+	// setup signal catching
+	sigs := make(chan os.Signal, 1)
+	// catch all signals since not explicitly listing
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	// method invoked upon seeing signal
+	go func() {
+		s := <-sigs
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		os.Exit(1)
+	}()
+}
+
+func main() {
+	for {
+		timeGet(*args.server)
+	}
+}
