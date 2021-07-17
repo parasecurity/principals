30fd1869a62fc3868cf984f2234d7918d1dedb25
GNtousakis <gntousakis@isc.tuc.gr>
2021-07-17 17:09:35 +0300

    Update detector

--- demos/common/images/antrea-tsi/tools/httpCanary/httpCanary.go
+++ demos/common/images/antrea-tsi/tools/httpCanary/httpCanary.go
@@ -15,20 +15,55 @@ import (
 var (
 	server       *string
 	detectorIP   *string
+	detectorPort *int
 	threshold    *int
 	failures     *int
 	logPath      *string
 	failureCount int
 	ips          []net.IP
 	conn         net.Conn
+	localaddr    net.TCPAddr
+	remoteaddr   net.TCPAddr
 )
 
+func getInterfaceIpv4Addr(interfaceName string) (addr string, err error) {
+	var (
+		ief      *net.Interface
+		addrs    []net.Addr
+		ipv4Addr net.IP
+	)
+	if ief, err = net.InterfaceByName(interfaceName); err != nil { // get interface
+		return
+	}
+	if addrs, err = ief.Addrs(); err != nil { // get addresses
+		return
+	}
+	for _, addr := range addrs { // get ipv4 address
+		if ipv4Addr = addr.(*net.IPNet).IP.To4(); ipv4Addr != nil {
+			break
+		}
+	}
+	if ipv4Addr == nil {
+		return "", nil
+	}
+	return ipv4Addr.String(), nil
+}
+
 func connectTCP() net.Conn {
+	// Get net1 interface ip
+	ip, _ := getInterfaceIpv4Addr("net1")
+
+	localaddr.IP = net.ParseIP(ip)
+	localaddr.Port = 6000
+	remoteaddr.IP = net.ParseIP(*detectorIP)
+	remoteaddr.Port = *detectorPort
+
 	// Connect to the detector
-	connection, err := net.Dial("tcp", *detectorIP)
+	connection, err := net.DialTCP("tcp", &localaddr, &remoteaddr)
 	for err != nil {
 		log.Println("Trying to connect to detector...")
-		connection, err = net.Dial("tcp", *detectorIP)
+		localaddr.Port = localaddr.Port + 1
+		connection, err = net.DialTCP("tcp", &localaddr, &remoteaddr)
 		if err != nil {
 			log.Println(err)
 			continue
@@ -45,7 +80,8 @@ func sendIP(ip string) {
 		// If connection closes we try again
 		log.Println(err)
 		log.Println("Reopening connection")
-		conn, err = net.Dial("tcp", *detectorIP)
+		localaddr.Port = localaddr.Port + 1
+		conn, err = net.DialIP("tcp", &localaddr, &remoteaddr)
 		if err != nil {
 			log.Println(err)
 			continue
@@ -107,7 +143,8 @@ func timeGet(urlC string) {
 
 func init() {
 	server = flag.String("conn", "http://kronos.mhl.tuc.gr:30001/health/", "The server url e.g. http://147.27.39.116:8080/health/")
-	detectorIP = flag.String("d", "10.1.1.202:30000", "The detector IP address e.g. 10.1.1.202:30002")
+	detectorIP = flag.String("d", "10.1.1.202", "The detector IP address e.g. 10.1.1.202")
+	detectorPort = flag.Int("p", 30000, "The detector port address e.g. 30002")
 	threshold = flag.Int("t", 1000, "The time threshold in ms")
 	failures = flag.Int("f", 4, "The number of failures before we spawn a detector")
 	logPath = flag.String("lp", "./canary.log", "The path to the log file")
