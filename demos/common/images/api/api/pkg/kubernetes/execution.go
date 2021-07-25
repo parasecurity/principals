a36308203464e1a26e276a2c9e26f1cd50b023f8
vagrant <vagrant@testvm>
2021-07-25 12:11:57 +0000

    Add execute command on API

--- demos/common/images/api/api/pkg/kubernetes/execution.go
+++ demos/common/images/api/api/pkg/kubernetes/execution.go
@@ -6,6 +6,8 @@ func Execute(command Command, registry *string) string {
 		Create(command, registry)
 	} else if command.Action == "delete" {
 		Delete(command)
+	} else if command.Action == "execute" {
+		Run(command, registry)
 	}
 	result = "ok"
 	return result
