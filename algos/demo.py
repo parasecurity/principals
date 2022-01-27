7f4418acfa8d74fe3754b51c7c3889ed7bde6fef
Spiros Antonatos <antonatos@gmail.com>
2022-01-27 22:43:16 +0200

    stucco python

---
+++ algos/demo.py
@@ -0,0 +1,7 @@
+from stucco import ContrastSetLearner
+from seaborn import load_dataset
+
+frame = load_dataset('diamonds')
+learner = ContrastSetLearner(frame, group_feature='color')
+
+learner.learn(max_length=3)
