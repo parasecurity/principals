eb04dd47326390a9ffc222711d031910838404b6
GNtousakis <greco1996@gmail.com>
2024-03-08 14:36:31 +0200

    Multiple addons
    Signed-off-by: GNtousakis <greco1996@gmail.com>

--- images/logging/cmd/log-agent/agent.go
+++ images/logging/cmd/log-agent/agent.go
@@ -8,15 +8,26 @@ import (
 	"syscall"
 	"io"
 	"bufio"
+	"fmt"
+	"time"
 	"log"
 )
 
 var (
+
+	control struct {
+		req chan struct{}
+		ans chan struct{}
+	}
+
+	srv *net.TCPConn
+	srvAddr *net.TCPAddr
+
 	args struct {
 		logServer *string
 		nodeName *string
-		sockPath *string
 	}
+
 )
 
 func handleConnection(c *net.UnixConn, logs chan []byte){
@@ -32,12 +43,8 @@ func handleConnection(c *net.UnixConn, logs chan []byte){
 
 		switch err {
 		case nil:
-			log.Println(string(data))
 			logs<- data
 		case io.EOF:
-			// if len(data) != 0 {
-				// logs<- data
-			// }
 			break serving
 		default:
 			// TODO check here!
@@ -46,36 +53,9 @@ func handleConnection(c *net.UnixConn, logs chan []byte){
 	}
 }
 
-
-
-func init() {
-
-	args.logServer = flag.String("logserveraddr", "localhost:4321", "The logging server listening connection in format ip:port")
-	args.nodeName = flag.String("nodename", "localhost", "The node hostname")
-	args.sockPath = flag.String("sockpath", "/tmp/testlog.sock", "path to agent's socket")
-	flag.Parse()
-
-	log.SetFlags(log.Ldate|log.Lmicroseconds|log.LUTC)
-	// Setup signal catching
-	sigs := make(chan os.Signal, 1)
-	// Catch all signals since not explicitly listing
-	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
-	// Method invoked upon seeing signal
-	go func() {
-		s := <-sigs
-		log.Println("RECEIVED SIGNAL: ", s)
-		log.Println("Removing socket")
-		if err := os.RemoveAll(*args.sockPath); err != nil {
-			println(err)
-		}
-		log.Println("Exiting")
-		os.Exit(1)
-	}()
-}
-
 func listenUnixAndServe(logs chan []byte) {
 	network := "unixpacket"
-	path := *args.sockPath
+	path := "/tmp/testlog.sock"
 	sock_addr, err := net.ResolveUnixAddr(network, path)
 
 	if err := os.RemoveAll(path); err != nil {
@@ -87,31 +67,109 @@ func listenUnixAndServe(logs chan []byte) {
 		println(err)
 	}
 
-	defer func(l *net.UnixListener) {
-		if err := listener.Close(); err != nil {
-			log.Println(err)
-		}
-		if err := os.RemoveAll(*args.sockPath); err != nil {
-			log.Println(err)
-		}
-	}(listener)
-
 	for {
 		cli, err := listener.AcceptUnix()
 		if err != nil {
-			log.Println("Accept failed:", err.Error())
+			println("Accept failed:", err.Error())
 			break
 		}
-		log.Println("Connection open:  ", cli.RemoteAddr())
+		println("Connection open: %s ", cli.RemoteAddr())
 		go handleConnection(cli, logs)
+	}
+	listener.Close()
+}
+
+func fixit() {
+	control.req<- struct{}{}
+	<-control.ans
+}
+
+func stateCheck() {
+	for {
+		<-control.req
+		n, err := fmt.Fprintf(srv, "%s ping", *args.nodeName)
+		n = n
+		if err != nil {
+			srv.Close()
+			err = connectToServer(0)
+		}
+		control.ans<- struct{}{}
 	}
 }
 
+func init() {
 
+	args.logServer = flag.String("logserveraddr", "localhost:4321", "The logging server listening connection in format ip:port")
+	args.nodeName = flag.String("nodename", "localhost", "The node hostname")
+	flag.Parse()
+
+	control.req = make(chan struct{})
+	control.ans = make(chan struct{})
+
+	var err error
+	srvAddr, err = net.ResolveTCPAddr("tcp4", *args.logServer)
+	err = connectToServer(10)
+	if err != nil {
+		println("failed to connect to server")
+		println(err)
+		os.Exit(1)
+	} else {
+		println("connection to server established")
+	}
+
+	// Setup signal catching
+	sigs := make(chan os.Signal, 1)
+	// Catch all signals since not explicitly listing
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	// Method invoked upon seeing signal
+	go func() {
+		s := <-sigs
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		os.Exit(1)
+	}()
+}
+
+func serverWriter(logs chan []byte){
+	defer func() {
+		// TODO
+		println("Server connection closed")
+		srv.Close()
+	}()
+
+	// writer := bufio.NewWriter(c)
+
+	for {
+		msg := <-logs
+		// TODO
+		for {
+			_, err := fmt.Fprintf(srv, "%s %s", *args.nodeName, msg)
+			if err != nil {
+				fixit()
+			} else {
+				break
+			}
+		}
+		// writer.Flush()
+	}
+}
+
+// for negative or 0 retries loops forever
+func connectToServer(retries int) (err error){
+	for i := retries; i != 1; i-- {
+		srv, err = net.DialTCP("tcp4", nil, srvAddr)
+		if err == nil {
+			break
+		}
+		// TODO log the error maybe
+		time.Sleep(1 * time.Second)
+	}
+	return
+}
 
 func main () {
 
 	logs := make(chan []byte, 128)
+	go stateCheck()
 	go serverWriter(logs)
 	listenUnixAndServe(logs)
 }
